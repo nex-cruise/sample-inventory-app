fe8f47139191a160f54a0d42884c65576ed122b6
Murugan Nagarajan <murugan425@gmail.com>
2020-10-29 21:20:57 +0530

    Created the service base version using https://start.spring.io

---
+++ src/main/java/com/tamil/mts/mtsinventoryms/MtsInventoryMsApplication.java
@@ -0,0 +1,13 @@
+package com.tamil.mts.mtsinventoryms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsInventoryMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtsInventoryMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamil/mts/mtsinventoryms/MtsInventoryMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamil.mts.mtsinventoryms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtsInventoryMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
