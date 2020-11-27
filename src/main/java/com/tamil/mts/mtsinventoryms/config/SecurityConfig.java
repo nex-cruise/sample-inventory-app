17746eb3ffe9e36a55296a4fa0311dd85b91df17
Murugan Nagarajan <murugan425@gmail.com>
2020-11-27 20:22:50 +0530

    Ant Matchers for GET request of a resource request to bypass the spring security.

--- src/main/java/com/tamil/mts/mtsinventoryms/config/SecurityConfig.java
+++ src/main/java/com/tamil/mts/mtsinventoryms/config/SecurityConfig.java
@@ -6,6 +6,7 @@
 package com.tamil.mts.mtsinventoryms.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,9 +21,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests(authorize -> authorize.antMatchers("/").permitAll())
-			.authorizeRequests().anyRequest()
-				.authenticated().and().formLogin().and().httpBasic();
+		http.authorizeRequests(authorize -> {
+			authorize.antMatchers("/").permitAll()
+					// Providing access to contact resources GET without authentication.
+					.antMatchers(HttpMethod.GET, "/mts/api/v1/contact/*").permitAll();
+		}).authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
 	}
 
 }
