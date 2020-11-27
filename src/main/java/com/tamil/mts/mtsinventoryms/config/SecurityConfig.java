d90e5718b4aedb58fa185882f179ef21d0c0ca98
Murugan Nagarajan <murugan425@gmail.com>
2020-11-27 18:57:16 +0530

    Adding explicit security config instead of default spring security configuration.

---
+++ src/main/java/com/tamil/mts/mtsinventoryms/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+/*
+ * Created on 27-Nov-2020
+ * Created by murugan
+ * Copyright � 2020 MTS [murugan425]. All Rights Reserved.
+ */
+package com.tamil.mts.mtsinventoryms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author murugan
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests(authorize -> authorize.antMatchers("/").permitAll())
+			.authorizeRequests().anyRequest()
+				.authenticated().and().formLogin().and().httpBasic();
+	}
+
+}
