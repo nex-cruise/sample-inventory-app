8b92fba9a2344a6b2f5a9594364776bd620b62c8
Murugan Nagarajan <murugan425@gmail.com>
2020-12-03 01:44:45 +0530

    Explicity configuring the custom user details service to be used by spring auth manager. Note: Not mandatory.

--- src/main/java/com/tamil/mts/mtsinventoryms/config/JpaAuthWebSecurityConfig.java
+++ src/main/java/com/tamil/mts/mtsinventoryms/config/JpaAuthWebSecurityConfig.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,12 +36,13 @@ import lombok.RequiredArgsConstructor;
  */
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class JpaAuthWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Value("${mts.security.salt}")
 	private String salt;
 
-//	private final UserDetailsAuthService userDetailsAuthService;
+	private final UserDetailsAuthService userDetailsAuthService;
 
 	@Bean
 	PasswordEncoder pswdEncoder() {
@@ -76,10 +76,10 @@ public class JpaAuthWebSecurityConfig extends WebSecurityConfigurerAdapter {
 		}).authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
 	}
 
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.userDetailsService(userDetailsAuthService).passwordEncoder(pswdEncoder());
-//	}
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsAuthService).passwordEncoder(pswdEncoder());
+	}
 
 	@SuppressWarnings({ "deprecation", "unused" })
 	private List<UserDetails> buildUserDetails() {
